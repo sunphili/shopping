aa52a26e25bd12059ac2183ca71efe52e9ea6922
111 <15724755326@163.com>
2017-04-06 18:25:18 +0800

    Signed-off-by: 111 <15724755326@163.com>

--- js/main.js
+++ js/main.js
@@ -148,7 +148,7 @@ m1.controller('detailCtrl',function($scope,local,$state,$stateParams){
 m1.directive('runoobDirective',function(){
 	return{
 		restrict:'AE',
-		template : "<h1>自定义指令!</h1>"
+		template : "<div class='navbar navbar-fixed-top bg-green'>自定义指令!</div>"
 	}
 })
 m1.directive('pageFooter',function(){
